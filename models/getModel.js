eeff419b03c80f09c38ddae4668206089db9f924
Jeppe Bjelke Knudsen Vad <jbva.skp@edu.mercantec.dk>
2022-06-13 10:12:43 +0200

    listBackend Done, plus some cleanup

--- models/getModel.js
+++ models/getModel.js
@@ -3,8 +3,8 @@ const pool = require("../config/database");
 class GetAllFromTable {
   async GetAllFromTable(table) {
     return new Promise(async (resolve, reject) => {
-      const sql = `SELECT * FROM ?`;
-      pool.query(sql,[table], function(err, result) {
+      const sql = `SELECT * FROM ${table}`;
+      pool.query(sql, function(err, result) {
         if (err) {
           reject(err);
         } else {
